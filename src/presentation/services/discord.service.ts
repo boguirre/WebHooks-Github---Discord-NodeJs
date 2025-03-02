bef67f542e3bb7753ff94bde89ad0d8c86ac59f7
Bruno Aguirre <boguirre@gmail.com>
2025-03-01 22:52:13 -0500

    Seccion 23 webhooks completed

---
+++ src/presentation/services/discord.service.ts
@@ -0,0 +1,35 @@
+import { envs } from "../../config";
+
+
+export class DiscordService {
+
+    private readonly discordWebhookUrl = envs.DISCORD_WEBHOOK_URL;
+
+    constructor(){}
+
+    async notify( message: string ){
+
+        const body = {
+            content: message,
+            // embeds: [
+            //     {
+            //         image: { url: 'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbWNqaGhzZWthd2YwMWNlZHVhajN3aXc4NzN5MXRlcDVjcXR1bHc3MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/iJJ6E58EttmFqgLo96/giphy.gif'}
+            //     }
+            // ]
+        }
+
+        const resp = await fetch(this.discordWebhookUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(body),
+        });
+
+        if( !resp.ok ){
+            console.log('Error sending message to discord');
+            return false;
+        }
+
+        return true;
+    }
+
+}
